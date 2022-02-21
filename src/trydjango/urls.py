9a5bf83a97afb144ae6e858ebe17434682997102
mohit-kumar-k <mohit.katragadda@springml.com>
2022-02-20 23:23:14 -0700

    forms and validation

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import contact_view, home_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',home_view),
     path('home/',home_view),
     path('product/',product_detail_view),
+    path('create/',product_create_view),
     path('contact/', contact_view,name = "contact info")
 
 ]
